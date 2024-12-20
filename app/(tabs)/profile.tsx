90a1e0c35a80845834c60a759f5c2d9f6485c07a
MAC <mac@MACs-MacBook-Pro.local>
2024-12-19 22:51:16 -0800

    feat:added a bottom sheet to the profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,29 +1,110 @@
-import { router } from 'expo-router';
+/* eslint-disable import/order */
+import React, { useCallback, useRef, useMemo } from 'react';
+import { StyleSheet, View, Text, ImageBackground, Pressable } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import * as SecureStore from 'expo-secure-store';
-import React from 'react';
-import { View, Text, Pressable, Image } from 'react-native';
+import { router } from 'expo-router';
 
-const Profile = () => {
+const App = () => {
+  // hooks
+  const sheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
+
+  // callbacks
+  const handleSheetChange = useCallback((index: number) => {
+    console.log('handleSheetChange', index);
+  }, []);
+  const handleSnapPress = useCallback((index: number) => {
+    sheetRef.current?.snapToIndex(index);
+  }, []);
+  const handleClosePress = useCallback(() => {
+    sheetRef.current?.close();
+  }, []);
+
   const handleLogout = async () => {
     await SecureStore.deleteItemAsync('token');
     console.log('Logged out');
     router.replace('/(auth)/signin');
   };
 
+  // render
   return (
-    <View>
-      <Image
+    <GestureHandlerRootView style={styles.container}>
+      <ImageBackground
         source={{
           uri: 'https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=600',
         }}
-        className="h- full w-full object-cover object-top"
-      />
-
-      <Pressable onPress={handleLogout}>
-        <Text>Logout</Text>
-      </Pressable>
-    </View>
+        style={styles.imageBackground}>
+        <View style={styles.overlay}>
+          <Pressable style={styles.openButton} onPress={() => handleSnapPress(0)}>
+            <Text style={styles.buttonText}>Open Profile</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enableDynamicSizing={false}
+        onChange={handleSheetChange}>
+        <BottomSheetView style={styles.contentContainer} className="relative">
+          <Pressable style={styles.logoutButton} onPress={handleLogout}>
+            <Text style={styles.buttonText}>Logout</Text>
+          </Pressable>
+          <Pressable
+            style={styles.closeButton}
+            onPress={handleClosePress}
+            className=" absolute right-2 top-0">
+            <Text style={styles.buttonText}>x</Text>
+          </Pressable>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 };
 
-export default Profile;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    bottom: '10%',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: 'center',
+  },
+  openButton: {
+    backgroundColor: '#007AFF',
+    padding: 12,
+    borderRadius: 8,
+  },
+  logoutButton: {
+    backgroundColor: '#FF3B30',
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  closeButton: {
+    backgroundColor: '#4CD964',
+    padding: 10,
+    borderRadius: '50%',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default App;
--- components/onBoarding-btn.tsx
+++ components/onBoarding-btn.tsx
@@ -6,7 +6,7 @@ const OnBoardingBtn = ({ screen, title }: { screen: RelativePathString | any; ti
   return (
     <TouchableOpacity
       onPress={() => router.replace(screen)}
-      className=" mx-auto mt-10 w-40 h-16  items-center justify-center rounded-xl bg-green-500 px-4 py-2">
+      className=" mx-auto mt-10 h-16 w-40  items-center justify-center rounded-xl bg-green-500 px-4 py-2">
       <Text className="text-lg font-bold text-white">{title}</Text>
     </TouchableOpacity>
   );
